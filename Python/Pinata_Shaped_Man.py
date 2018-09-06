b83926e9ce545160c570f6169fafdc2ac4146efc
Vincent <vpearce14@charlottesvilleschools.org>
2018-09-06 14:53:20 -0400

    day 2

--- Python/Pinata_Shaped_Man.py
+++ Python/Pinata_Shaped_Man.py
@@ -1,6 +1,19 @@
 # Pinata Shaped Man
 # Written by Vincent
 
-hangman = ("----","   |","   ^","  <0__/","   |==|")
-for i in range (0, len(hangman)):
-    print (hangman[i])
+
+word = input('Hey player 1! Type in a word!')
+print ("\n" *50)
+n = False
+while (n == False):
+    hangman = ("----","   |","   ^","  <0__/","   |==|")
+    for i in hangman:
+        print (i)
+    for x in range (0, len(word)):
+        print ("_ ", end = "")
+    letter = input('Hey player 2! guess a letter!')
+    for x in range (0, len(word)):
+        if (letter[0] == word[x]):
+            print(letter, end = " ")
+        else:
+            print ("_ ", end = "")
