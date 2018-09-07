24df67ae77ddf82fb29806693f97974c417f4e72
Vincent <vpearce14@charlottesvilleschools.org>
2018-09-07 11:38:38 -0400

    finished th game in its basic stage, will come back to add complexity

--- Python/Pinata_Shaped_Man.py
+++ Python/Pinata_Shaped_Man.py
@@ -4,16 +4,39 @@
 
 word = input('Hey player 1! Type in a word!')
 print ("\n" *50)
-n = False
-while (n == False):
-    hangman = ("----","   |","   ^","  <0__/","   |==|")
-    for i in hangman:
-        print (i)
-    for x in range (0, len(word)):
-        print ("_ ", end = "")
+n = 1
+guess = ["-"]
+strin = ""
+wrongGuesses = 0
+for x in range (1, len(word)):
+   guess.append("-")
+for x in guess:
+        strin += x
+print(strin)
+while (n == 1):
+    strin = ""
+    wrong = True
     letter = input('Hey player 2! guess a letter!')
     for x in range (0, len(word)):
         if (letter[0] == word[x]):
-            print(letter, end = " ")
+            guess[x]=letter[0]
+            wrong = False
+    for x in guess:
+        strin += x
+    if (wrong == True):
+        hangman = ("----","   |","   ^","  <0__/","   |==|")
+        for i in range (0,wrongGuesses):
+            print(hangman[i])
+        wrongGuesses += 1
+        if (wrongGuesses == 6):
+            n = 3
         else:
-            print ("_ ", end = "")
+            print ("That letter is not in the word")
+    print(strin)
+    if (strin == word):
+        n = 2
+    
+if (n==2):
+    print ("You win!!!")
+if (n==3):
+    print ("You lose :(")
