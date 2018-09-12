9cc4415593c6414ad6cee17256e297fa65c3e68b
Vincent <vpearce14@charlottesvilleschools.org>
2018-09-12 15:54:40 -0400

    Sarted the GPIO pins assignment, got it working with two led

--- Scripts/GPIOpins.sh
+++ Scripts/GPIOpins.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 gpio mode 0 out
+gpio mode 1 out
 i=1
 until [ $i == 11 ]
 do
 	gpio write 0 1
+	gpio write 1 1
 	sleep 1
 	gpio write 0 0
+	gpio write 1 0
 	sleep 1
 	i=$((i + 1))
 done
