abe08037e12c6867a2cf0e8d5ebda15e49f5ef15
Vincent <vpearce14@charlottesvilleschools.org>
2018-09-24 14:53:08 -0400

    got the two checkboxes to tuen the lights on

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -12,15 +12,19 @@ app = Flask(__name__)
 def index():
 	if request.method == "POST":
                 msg = request.form.get("submitBtn1")
+		if msg == "You clicked!":
+                	GPIO.output(17,GPIO.HIGH)
+		else:
+			GPIO.output(17,GPIO.HIGH)
 		msg = request.form.get("submitBtn2")
+		if msg == "You clicked2!":
+			GPIO.output(18,GPIO.HIGH)
+		else:
+			GPIO.output(18,GPIO.LOW)
 	else:
 		GPIO.output(17,GPIO.LOW)
 		GPIO.output(18,GPIO.LOW)
 		msg = "No click yet."
-	if msg == "You clicked!":
-		GPIO.output(17,GPIO.HIGH)
-	elif msg == "You clicked2!":
-		GPIO.output(18,GPIO.HIGH)
 	return render_template("index.html", msg=msg)
 
 if __name__ == "__main__":
