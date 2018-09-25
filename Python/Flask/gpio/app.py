206f9605599fe7ee2bd5e202988e0db72be0d565
Vincent <vpearce14@charlottesvilleschools.org>
2018-09-25 12:08:06 -0400

    this would work if the code was able to permanently store any values. It can't, so it doesn't work

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -14,18 +14,23 @@ def index():
                 msg = request.form.get("submitBtn1")
 		if msg == "You clicked!":
                 	GPIO.output(17,GPIO.HIGH)
-		else:
-			GPIO.output(17,GPIO.HIGH)
+			msg1 = 'h'
+		elif msg == "You unclicked!":
+			GPIO.output(17,GPIO.LOW)
+			msg1 = 'l'
 		msg = request.form.get("submitBtn2")
 		if msg == "You clicked2!":
 			GPIO.output(18,GPIO.HIGH)
-		else:
+			msg2 = 'h'
+		elif msg == "You unclicked2!":
 			GPIO.output(18,GPIO.LOW)
+			msg2 = 'l'
 	else:
 		GPIO.output(17,GPIO.LOW)
 		GPIO.output(18,GPIO.LOW)
-		msg = "No click yet."
+		msg1 = 'l'
+		msg2 = 'l'
+	msg= msg1 + msg2
 	return render_template("index.html", msg=msg)
-
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
