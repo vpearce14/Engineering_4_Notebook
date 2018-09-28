4bcca4b44c6c625221c6793cf3baf7ffa0f1494d
Vincent <vpearce14@charlottesvilleschools.org>
2018-09-28 11:43:36 -0400

    not working!

--- Python/Flask/gpio/app.py
+++ Python/Flask/gpio/app.py
@@ -12,25 +12,24 @@ app = Flask(__name__)
 def index():
 	if request.method == "POST":
                 msg = request.form.get("submitBtn1")
-		if msg == "You clicked!":
-                	GPIO.output(17,GPIO.HIGH)
-			msg1 = 'h'
-		elif msg == "You unclicked!":
-			GPIO.output(17,GPIO.LOW)
-			msg1 = 'l'
-		msg = request.form.get("submitBtn2")
-		if msg == "You clicked2!":
-			GPIO.output(18,GPIO.HIGH)
-			msg2 = 'h'
-		elif msg == "You unclicked2!":
-			GPIO.output(18,GPIO.LOW)
-			msg2 = 'l'
+		print msg
+		if msg != None:
+			if msg[0]=='1':
+				if msg[1] == 'l':
+					GPIO.output(17,GPIO.LOW)
+				if msg[1] == 'h':
+					GPIO.output(17,GPIO.HIGH)
+			if msg[0]=='2':
+				if msg[1] == 'l':
+					GPIO.output(18,GPIO.LOW)
+				if msg[1] == 'h':
+					GPIO.output(18,GPIO.HIGH)
 	else:
 		GPIO.output(17,GPIO.LOW)
 		GPIO.output(18,GPIO.LOW)
-		msg1 = 'l'
-		msg2 = 'l'
-	msg= msg1 + msg2
+	msg1 = GPIO.input(17)
+        msg2 = GPIO.input(18)
+	msg= str(msg1) + str(msg2)
 	return render_template("index.html", msg=msg)
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=80)
